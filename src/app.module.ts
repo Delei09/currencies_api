4d5c539621507577b493bc9e472a03cec73a5d94
Vanderlei Lemos <vanderlei.lemos@inatel.br>
2024-10-26 09:16:47 -0300

    feat: create table and add typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { Users } from './users/users.entity';
+
+  dotenv.config();
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST_DB,
+      port: parseInt(process.env.PORT_DB),
+      username: process.env.USERNAME_DB,
+      password: process.env.PASSWORD_DB,
+      database: process.env.NAME_DB,
+      entities: [Users],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ src/users/users.entity.ts
@@ -0,0 +1,19 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Users {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  currenciesFavorite: string;
+}
